d12210baf4c9ea264f3e38e9e6fe396756c3c578
Dan Corder <dev@dancorder.com>
2017-12-03 10:42:59 +0000

    Day 3 part 2 (and improved part 1)

--- Day03.cs
+++ Day03.cs
@@ -40,7 +40,75 @@ namespace advent_of_code_2017
 
         public static int SolveProblem1_2(int value)
         {
-            var minSquare = Math.Sqrt(value);
+            var coords = positionToXY(value);
+
+            return Math.Abs(coords.Item1) + Math.Abs(coords.Item2);
+        }
+
+        public static int SolveProblem2()
+        {
+            // Console.WriteLine(positionToXY(2));
+            // Console.WriteLine(positionToXY(3));
+            // Console.WriteLine(positionToXY(4));
+            // Console.WriteLine(positionToXY(5));
+            // Console.WriteLine(positionToXY(6));
+            // Console.WriteLine(positionToXY(7));
+            // Console.WriteLine(positionToXY(8));
+            // Console.WriteLine(positionToXY(9));
+            // Console.WriteLine(positionToXY(10));
+            // Console.WriteLine(positionToXY(11));
+            // Console.WriteLine(positionToXY(12));
+
+            // Console.WriteLine(XYToPosition(new Tuple<int, int>(1, 0)));
+            // Console.WriteLine(XYToPosition(new Tuple<int, int>(1, 1)));
+            // Console.WriteLine(XYToPosition(new Tuple<int, int>(0, 1)));
+            // Console.WriteLine(XYToPosition(new Tuple<int, int>(-1, 1)));
+            // Console.WriteLine(XYToPosition(new Tuple<int, int>(-1, 0)));
+            // Console.WriteLine(XYToPosition(new Tuple<int, int>(-1, -1)));
+            // Console.WriteLine(XYToPosition(new Tuple<int, int>(0, -1)));
+            // Console.WriteLine(XYToPosition(new Tuple<int, int>(1, -1)));
+            // Console.WriteLine(XYToPosition(new Tuple<int, int>(2, -1)));
+            // Console.WriteLine(XYToPosition(new Tuple<int, int>(2, 0)));
+            // Console.WriteLine(XYToPosition(new Tuple<int, int>(2, 1)));
+
+            var limit = 325489;
+            var valuesByPosition = new Dictionary<int, int>();
+            valuesByPosition[1] = 1;
+
+            var position = 2;
+            while(true) {
+                var value = calculateValue(position, valuesByPosition);
+                //Console.WriteLine(value);
+
+                if (value > limit) {
+                    //343767 too high
+                    return value;
+                }
+
+                valuesByPosition[position] = value;
+
+                position++;
+            }
+        }
+
+        private static int calculateValue(int position, Dictionary<int, int> valuesByPosition)
+        {
+            var coords = positionToXY(position);
+            var value = 0;
+
+            for (int x = -1; x <= 1; x++)
+            {
+                for (int y = -1; y <= 1; y++)
+                {
+                    value += getValueAt(new Tuple<int, int>(coords.Item1 + x, coords.Item2 + y), valuesByPosition);
+                }
+            }
+            return value;
+        }
+
+        private static Tuple<int, int> positionToXY(int position)
+        {
+            var minSquare = Math.Sqrt(position);
             var minWholeSquare = (int)Math.Ceiling(minSquare);
             if (minWholeSquare % 2 == 0) {
                 minWholeSquare++;
@@ -48,24 +116,78 @@ namespace advent_of_code_2017
 
             var highestInSquare = minWholeSquare * minWholeSquare;
             var sideLength = minWholeSquare;
-            var difference = highestInSquare - value;
+            var difference = highestInSquare - position;
 
-            var distanceFromCorner = difference;
-            while (distanceFromCorner >= (sideLength - 1)) {
-                distanceFromCorner = distanceFromCorner - sideLength + 1;
-            }
-            if (distanceFromCorner > ((sideLength - 1) / 2))
+            var sideIndex = difference / (sideLength - 1); // 0 = bottom, 1 = left, 2 = top, 3 = right
+            var sideDistanceFromCorner = difference % (sideLength - 1);
+
+            int corner4X = (sideLength - 1) / 2;
+            int corner4Y = -corner4X;
+            int x;
+            int y;
+
+            switch(sideIndex)
             {
-                distanceFromCorner = sideLength - 1 - distanceFromCorner;
+                case 0:
+                    x = corner4X - sideDistanceFromCorner;
+                    y = corner4Y;
+                break;
+                case 1:
+                    x = corner4X - sideLength + 1;
+                    y = corner4Y + sideDistanceFromCorner;
+                break;
+                case 2:
+                    x = corner4X - sideLength + 1 + sideDistanceFromCorner;
+                    y = corner4Y + sideLength - 1;
+                break;
+                case 3:
+                    x = corner4X;
+                    y = corner4Y + sideLength - 1 - sideDistanceFromCorner;
+                break;
+                default:
+                    throw new Exception("impossible");
             }
 
-            var distanceFromCornerToCentre = minWholeSquare - 1;
-
-            return distanceFromCornerToCentre - distanceFromCorner;
+            return new Tuple<int, int>(x,y);
         }
 
-        public static int SolveProblem2()
+        private static int XYToPosition(Tuple<int, int> coords)
         {
+            var x = coords.Item1;
+            var y = coords.Item2;
+            var sideLength = (Math.Max(Math.Abs(x), Math.Abs(y)) * 2) + 1;
+
+            int corner4X = (sideLength - 1) / 2;
+            int corner4Y = -corner4X;
+
+            var sideIndex = 0; // 0 = bottom, 1 = left, 2 = top, 3 = right
+            var sideDistanceFromCorner = 0;
+            if (x == corner4X && y != corner4Y) {
+                sideIndex = 3;
+                sideDistanceFromCorner = corner4Y + sideLength - 1 - y;
+            } else if (y == (corner4Y + sideLength - 1)) {
+                sideIndex = 2;
+                sideDistanceFromCorner = x - (corner4X - sideLength + 1);
+            } else if (x == (corner4X - sideLength + 1)) {
+                sideIndex = 1;
+                sideDistanceFromCorner = y - corner4Y;
+            } else {
+                sideIndex = 0;
+                sideDistanceFromCorner = corner4X - x;
+            }
+
+            var highestInSquare = sideLength * sideLength;
+
+            return highestInSquare - sideDistanceFromCorner - (sideIndex * (sideLength - 1));
+        }
+
+        private static int getValueAt(Tuple<int, int> coords, Dictionary<int, int> valuesByPosition)
+        {
+            var position = XYToPosition(coords);
+            if (valuesByPosition.ContainsKey(position))
+            {
+                return valuesByPosition[position];
+            }
             return 0;
         }
     }
