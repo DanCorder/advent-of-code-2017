2a5b7d6e9ff00bdc46549be10c1de21bd5357ee6
Dan Corder <dev@dancorder.com>
2017-12-04 22:56:53 +0000

    Day 4

--- Program.cs
+++ Program.cs
@@ -11,9 +11,9 @@ namespace advent_of_code_2017
             // Console.WriteLine(Day02.SolveProblem1());
             // Console.WriteLine(Day02.SolveProblem2());
             // Console.WriteLine(Day03.SolveProblem1());
-            Console.WriteLine(Day03.SolveProblem2());
+            // Console.WriteLine(Day03.SolveProblem2());
             // Console.WriteLine(Day04.SolveProblem1());
-            // Console.WriteLine(Day04.SolveProblem2());
+            Console.WriteLine(Day04.SolveProblem2());
             // Console.WriteLine(Day05.SolveProblem1());
             // Console.WriteLine(Day05.SolveProblem2());
             // Console.WriteLine(Day06.SolveProblem1());
