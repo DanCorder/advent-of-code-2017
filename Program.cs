5585ed38884c7e40240502ef771e2898ba4c5e3c
Dan Corder <dev@dancorder.com>
2017-12-18 18:57:03 +0000

    Optimised to take under 1 second (and tidied up a little)

--- Program.cs
+++ Program.cs
@@ -6,6 +6,7 @@ namespace advent_of_code_2017
     {
         public static void Main(string[] args)
         {
+            Console.WriteLine(DateTime.Now);
             //Console.WriteLine(Day01.SolveProblem1());
             // Console.WriteLine(Day01.SolveProblem2());
             // Console.WriteLine(Day02.SolveProblem1());
@@ -56,6 +57,7 @@ namespace advent_of_code_2017
             // Console.WriteLine(Day24.SolveProblem2());
             // Console.WriteLine(Day25.SolveProblem1());
             // Console.WriteLine(Day25.SolveProblem2());
+            Console.WriteLine(DateTime.Now);
         }
     }
 }
